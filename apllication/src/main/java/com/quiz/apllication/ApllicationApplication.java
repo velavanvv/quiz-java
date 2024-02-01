c8540b55ecd03039eefbcadadf44868bd1b2348f
velavanvv <velavanvelavan790@gmail.com>
2024-02-01 07:28:05 +0530

    created spring boot Allication

---
+++ apllication/src/main/java/com/quiz/apllication/ApllicationApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.apllication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApllicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApllicationApplication.class, args);
+	}
+
+}
---
+++ apllication/src/test/java/com/quiz/apllication/ApllicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.apllication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApllicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
